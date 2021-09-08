0cfd64d6623f51f112563a576ddd054791daf8a6
Senthil Kumar Dayalan <senthild@amazon.com>
2021-09-08 15:45:48 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Switch, Link } from "react-router-dom";
+import Products from "./components/products";
+import About from "./components/about";
+import Home from "./components/home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <strong>Uii Commerce</strong>
+          </div>
+          <ul className="nav justify-content-end">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="container">
+        <Switch>
+          <Route path="/products" component={Products} />
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/components/products.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class Products extends Component {
+  render() {
+    const { history, location, match } = this.props;
+    return <h1>Products</h1>;
+  }
+}
+
+export default Products;
